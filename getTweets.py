7bf6cf7bbb6a7cfa9ebc06b18199ba36d0e635e0
rroell <roelliie@hotmail.nl>
2022-11-22 17:38:40 +0100

    Initial query

---
+++ getTweets.py
@@ -0,0 +1,20 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+query = "(Hebe OR hebe) lang:nl until:2022-10-20 since:2022-10-17"
+tweets = []
+limit = 10
+
+for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+    # print(vars(tweet))
+    # break
+    if len(tweets) == limit:
+        break
+    else:
+        tweets.append([tweet.date, tweet.username, tweet.content])
+        
+df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
+print(df)
+
+# to save to csv
+# df.to_csv('tweets.csv')
