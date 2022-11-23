ff3d276aaa54a39a280bb3a4a8385327ba95f3cb
rroell <roelliie@hotmail.nl>
2022-11-23 15:25:30 +0100

    Main

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 # © Roel Duijsings
-from preprocess import getTweets
+from preprocess import preprocess
+from transformers import pipeline, RobertaForSequenceClassification, RobertaTokenizer
 
+# PARAMETERS
 query = "(Hebe OR hebe) lang:nl until:2022-10-21 since:2022-10-17"
 maxTweets = 10
-raw_data = getTweets(query, maxTweets)
 
-# Sort dates in chronological order
-raw_data.sort_values(by='Date', inplace=True)
+# PREPROCESS DATA
+# data = preprocess(query, maxTweets)
+
+# MODEL AND TOKENIZER
+tokenizer = RobertaTokenizer.from_pretrained("pdelobelle/robbert-v2-dutch-base")
+model = RobertaForSequenceClassification.from_pretrained(
+    "pdelobelle/robbert-v2-dutch-base", id2label={0: "negative", 1: "positive"} # change labels
+)
+
+# COMPUTE EMOTIONS
+emotion = pipeline("text-classification", model=model, tokenizer=tokenizer)
+# emotion_labels = emotion("Ik ben verdrietig!")
+
+emotion_labels = emotion("Ik ben super boos op jou")
+print(emotion_labels)
