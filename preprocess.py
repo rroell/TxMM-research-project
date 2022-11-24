6ab1340e39c6a6a7a4c8fbe77daf87cc6898faed
rroell <roelliie@hotmail.nl>
2022-11-24 14:23:30 +0100

    Added comments

--- preprocess.py
+++ preprocess.py
@@ -6,17 +6,19 @@ import pandas as pd
 
 def preprocess(query, maxTweets):
     """
-    Preprocess the tweets: 
-    - collect them from Twitter API, 
-    - replace username and URL by placeholders, 
+    Preprocess the tweets:
+    - collect them from Twitter API,
+    - replace username and URL by placeholders,
     - sort in chronological order,
     - save to tweets.csv.
+    
+    Returns a DataFrame["Date", "Username", "Raw_text", "Url","Text"]
     """
     df = getTweets(query, maxTweets)
-    df['Text'] = df["Raw_text"].map(replacePlaceholders)
+    df["Text"] = df["Raw_text"].map(replacePlaceholders)
 
     # Sort dates in chronological order
-    df.sort_values(by='Date', inplace=True)
+    df.sort_values(by="Date", inplace=True)
 
     # Save to csv
     df.to_csv("tweets.csv")
@@ -25,7 +27,7 @@ def preprocess(query, maxTweets):
 
 def getTweets(query, maxTweets):
     """
-    Gather tweets from Twitter API. Query and max nr of tweets are set here. 
+    Gather tweets from Twitter API. Query and max nr of tweets are set here.
 
     Returns a DataFrame that is in reverse order of date. Writes df to tweets.csv file.
     """
@@ -35,8 +37,7 @@ def getTweets(query, maxTweets):
         if len(tweets) == maxTweets:
             break
         else:
-            tweets.append([tweet.date, tweet.user.username,
-                          tweet.content, tweet.url])
+            tweets.append([tweet.date, tweet.user.username, tweet.content, tweet.url])
 
     df = pd.DataFrame(tweets, columns=["Date", "Username", "Raw_text", "Url"])
     df.index.name = "id"
@@ -50,7 +51,7 @@ def replacePlaceholders(tweet):
     """
     new_tweet = []
     for word in tweet.split(" "):
-        word = '@USER' if word.startswith('@') and len(word) > 1 else word
-        word = 'HTTP' if word.startswith('http') else word
+        word = "@USER" if word.startswith("@") and len(word) > 1 else word
+        word = "HTTP" if word.startswith("http") else word
         new_tweet.append(word)
     return " ".join(new_tweet)
