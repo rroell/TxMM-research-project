b5221095e3768d8bed91081f007b2093e1fb61c8
rroell <roelliie@hotmail.nl>
2022-12-10 14:46:28 +0100

    Collect 3490 tweets containing Hebe

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
 # © Roel Duijsings
 from preprocess import preprocess
-from transformers import pipeline, RobertaForSequenceClassification, RobertaTokenizer, AutoModelForSequenceClassification
+from transformers import (
+    pipeline,
+    RobertaForSequenceClassification,
+    RobertaTokenizer,
+    AutoModelForSequenceClassification,
+)
 import torch
+import time
+
+# start timer
+start_time = time.time()
 
 # PARAMETERS
 query = "(Hebe OR hebe) lang:nl until:2022-10-21 since:2022-10-17"
-maxTweets = 10
+maxTweets = 10000
 
 # PREPROCESS DATA
 data = preprocess(query, maxTweets)
-
+print(data.iloc[-1])
 # MODEL AND TOKENIZER
-tokenizer = RobertaTokenizer.from_pretrained("pdelobelle/robbert-v2-dutch-base")
-model = RobertaForSequenceClassification.from_pretrained(
-    "pdelobelle/robbert-v2-dutch-base", problem_type="multi_label_classification")#, id2label={0: "negative", 1: "positive"}) # change labels
+# tokenizer = RobertaTokenizer.from_pretrained("pdelobelle/robbert-v2-dutch-base")
 
-print("Numlabels:", model.config.num_labels)  
-# inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+# data.to_csv("labels.csv")
 
-# with torch.no_grad():
-#     logits = model(**inputs).logits
-
-# predicted_class_id = logits.argmax().item()
-# model.config.id2label[predicted_class_id]
-
-# # COMPUTE EMOTIONS
-emotion = pipeline('sentiment-analysis', model="pdelobelle/robbert-v2-dutch-base",top_k=None)
-# emotion_labels = emotion("Ik ben verdrietig!")
-# for tweet in 
-label_scores = emotion("I love you")[0]  # type: ignore
-print(label_scores)
-print([(label_score['label'],label_score['score']) for label_score in label_scores])  # type: ignore
+# Finished
+print("--- %s seconds ---" % (time.time() - start_time))
