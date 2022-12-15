7fc7299204cc360beb0bde18157bab684c5c3210
rroell <roelliie@hotmail.nl>
2022-12-15 16:06:13 +0100

    Add + delete new data files

--- test.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-classifier = pipeline("sentiment-analysis", model="pdelobelle/robbert-v2-dutch-base")
-res = classifier("Ik ben heel blij om hier met <mask> samen te zijn")
-
-print(res)
